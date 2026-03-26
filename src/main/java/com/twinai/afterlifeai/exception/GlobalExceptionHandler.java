c9cb2e877bf9b08c8bde46137e30f1e2a5a569ac
ClementWa0 <clementwa01@gmail.com>
2026-03-26 15:28:59 +0300

    feat(exception): create custom UserNotFoundException

---
+++ src/main/java/com/twinai/afterlifeai/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.twinai.afterlifeai.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Handle User Not Found
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleUserNotFound(UserNotFoundException ex) {
+        return new ErrorResponse(ex.getMessage(), 404);
+    }
+
+    // Handle Generic Exceptions
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleGeneralException(Exception ex) {
+        return new ErrorResponse("Something went wrong", 500);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidation(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+
+        return new ErrorResponse(message, 400);
+    }
+}
