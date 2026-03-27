ef403a41497c0cfdf328a63f172e0a12ac7a5006
ClementWa0 <clementwa01@gmail.com>
2026-03-27 17:53:14 +0300

    Update UserRepository for email check

--- src/main/java/com/twinai/afterlifeai/repository/UserRepository.java
+++ src/main/java/com/twinai/afterlifeai/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.twinai.afterlifeai.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.twinai.afterlifeai.model.User;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
